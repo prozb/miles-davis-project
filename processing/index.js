cf4ae532d18edca8179b3fa6fa9038273e725701
= <rozbich15@gmail.com>
2019-11-09 18:12:44 +0100

    created function to add albums into the database

--- processing/index.js
+++ processing/index.js
@@ -1,8 +1,10 @@
 const neo4j = require('neo4j-driver').v1;
 const fs    = require('fs');
 // reading music files to create database entries
-const musicFile = fs.readFileSync('./dataset/music.json')
-const musicians = JSON.parse(musicFile);
+const musicFile  = fs.readFileSync('./dataset/music.json');
+const musicians  = JSON.parse(musicFile);
+const albumsFile = fs.readFileSync('./dataset/albums.json');
+const albums     = JSON.parse(albumsFile);
 // reading configs
 const configFile = fs.readFileSync('./processing/config.json');
 const config     = JSON.parse(configFile);
@@ -17,8 +19,9 @@ try{
   driver  = neo4j.driver(url, neo4j.auth.basic(username, password));
   session = driver.session();
 
-  const instruments = getAllInstruments(musicians)
-  addInstrumentsToDatabase(instruments);
+  // const instruments = getAllInstruments(musicians)
+  // addInstrumentsToDatabase(instruments);
+  // addAllAlbumsIntoDB(albums);
 }catch (err){
   console.log('terminating program.')
   console.error(err)
@@ -36,6 +39,48 @@ function getAllInstruments(musicians) {
   return instruments;
 }
 
+// adding all albums into the database
+function addAllAlbumsIntoDB(albums){ 
+  const promises = [];
+
+  Object.entries(albums).forEach(album => {
+    promises.push(addAlbumIntoDB(album));
+  })
+
+  Promise.all(promises)
+  .then(result => {
+    console.log(`${result.length} albums added to the database`)
+  })
+  .catch(err => {
+    console.log('error occured');
+    console.log(err);
+  })
+  .finally(() => {
+    session.close();
+    driver.close();
+  })
+}
+
+async function addAlbumIntoDB(album){
+  try{
+    await session.run(
+      `create (n:Album {
+        name: $name, 
+        label: $label, 
+        released: $released,
+        url: $url
+      }) return n.name`,
+      {
+        name: album[0], 
+        label: album[1].label, 
+        released: album[1].released,
+        url: album[1].url
+      }
+    );
+  }catch(err){
+    console.log(err);
+  }
+}
 //adding all instruments into the database
 function addInstrumentsToDatabase(data){
   const promises = [];
@@ -59,7 +104,7 @@ function addInstrumentsToDatabase(data){
 // async function to add instrument into the database
 async function addInstrumentToDB(name){
   try{
-    instrument = await session.run(
+    await session.run(
       'create (n:Instrument {name: $name}) return n.name',
       {name: name}
     );
