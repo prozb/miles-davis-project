6ac7f78f729cd48658256e5795387420bf8dc1c2
= <rozbich15@gmail.com>
2019-12-11 15:25:48 +0100

    fixed styles of application bar

--- miles-web-app/src/components/SearchBar.js
+++ miles-web-app/src/components/SearchBar.js
@@ -1,52 +1,100 @@
-import React, { Component } from 'react';
-import '../styles/search-bar.css';
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
-import FormControl from '@material-ui/core/FormControl';
-import Input from '@material-ui/core/Input';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import InputLabel from '@material-ui/core/InputLabel';
 
-export default class SearchBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchQuery: 'Search...',
-      value: 'Search...',
-      mouseEntered: 'false'
-    };
-  }
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
+      },
+    },
+  },
+}));
 
-  handleChange = (text) => {
-    this.setState({searchQuery: text})
-  }
-  render() {
-    const {name} = this.props;
-    const searchClass = this.mouseEntered ? "hovered-input" : '';
-    return (
-      <div className="search-bar">
-        <div style={{flex: 6, paddingTop: 10}}>
-          <p className="search-bar-text">{name}</p>
-        </div>
-        {/* search bar container */}
-        <div className="search-bar-style" style={{alignItems: 'center', paddingRight: 15}}>
-          <FormControl>
-            <Input disableUnderline={true} 
-              style={{color: 'rgba(255, 255, 255, 0.5)', 
-                fontSize: 'large',
-                borderRadius: 10, 
-                padding: 7,
+export default function SearchAppBar(props) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            {props.name}
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
               }}
-              onMouseEnter={() => this.setState({mouseEntered: true})}
-              className={searchClass}
-              onChange={this.handleChange}
-              placeholder={this.state.value}
-              id="search-component"
-              startAdornment={<InputAdornment position="start"><SearchIcon style={{color: 'white'}} fontSize="large"/></InputAdornment>}
+              inputProps={{ 'aria-label': 'search' }}
             />
-          </FormControl>
-        </div>
-        {/* search bar container end */}
-      </div>
-    );
-  }
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
+  
