53041a305a58b79e0a7b9af5c1cef882d8d57ced
= <rozbich15@gmail.com>
2020-03-23 18:42:27 +0100

    Updates

---
+++ precache-manifest.ccadeec1fad29e15bd18ab6653e7c03f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ad8addb4f11fd54cae56a5b306e356",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d5886c126be4b7425f9",
+    "url": "/static/css/2.0f3508e0.chunk.css"
+  },
+  {
+    "revision": "3b45bba5f5b94e4a0c00",
+    "url": "/static/css/main.d8aa53aa.chunk.css"
+  },
+  {
+    "revision": "3d5886c126be4b7425f9",
+    "url": "/static/js/2.347dd359.chunk.js"
+  },
+  {
+    "revision": "3b45bba5f5b94e4a0c00",
+    "url": "/static/js/main.a57bcb69.chunk.js"
+  },
+  {
+    "revision": "c2a4bf7f6c52fe3f0f98",
+    "url": "/static/js/runtime-main.d45c083b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "d0dbf462c6fb9083ee40efd7b861cab9",
+    "url": "/static/media/safopone-jumbo.d0dbf462.jpg"
+  },
+  {
+    "revision": "0982db5ea0b8af918602dcbaeab8ec56",
+    "url": "/static/media/start-screen.0982db5e.png"
+  }
+]);
